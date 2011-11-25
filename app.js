b83cc07bee9da91d6d615060eaa889ef16597204
Patrick Carl <patrick@carl.it>
2011-11-25 21:53:44 +0100

    Read header x-forwarded-for for usage in proxy environments

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var http = require('http');
 var port = process.argv[2] || process.env.PORT || 80;
 
 http.createServer(function(req, res){
+	var ip;
+	if(typeof req.headers["x-forwarded-for"] === 'string')
+		ip = req.headers["x-forwarded-for"];
+	else
+		ip = req.connection.remoteAddress;
 	res.writeHead(200, { 'Content-Type': 'text/plain'}); 
-	res.end(req.connection.remoteAddress);
+	res.end(ip);
 }).listen(port);
 
 console.log('Get-IP-Server listening on ' + port);
